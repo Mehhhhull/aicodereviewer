5bb69738740b21bf567c32d3272c081628ef55e2
Mehul Kumar Singh <mehulkumarsingh.2004@gmail.com>
2025-10-15 22:46:31 +0530

    Made the UI

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -8,6 +8,13 @@ function App() {
 
   return (
   <>
+  <main>
+    <div className="left">
+      <div className="code"></div>
+      <div className="review">Review</div>
+    </div>
+    <div className="right"></div>
+  </main>
   </>
   )
 }
